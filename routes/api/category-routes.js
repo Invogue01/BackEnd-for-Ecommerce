a2bb236c5195dfca58c88b2a7b6c46ff96321664
Andrea Mac <aleatrengrouse@gmail.com>
2022-03-24 18:45:00 +0000

    finished writing category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,36 +1,85 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+    },
+  });
   try {
-    const categoryData = await Category.findAll()
-
-    res.status(200).json(categoryData)
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({error: "Failed to get requested categories"})
+    console.log(error.message);
+    res.status(500).json({ error: "Failed to get requested categories" });
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+    },
+  });
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Failed to get requested categories" });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+  Category.create({
+    category_name:req.body.category_name
+  })
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Failed to get requested categories" });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  Category.update(req.body,{
+    where:{
+      id:req.params.id
+    }
+    
+  })
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Failed to get requested categories" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+where:{
+  id:req.params.id
+}
+  })
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Failed to get requested categories" });
+  }
 });
 
 module.exports = router;
